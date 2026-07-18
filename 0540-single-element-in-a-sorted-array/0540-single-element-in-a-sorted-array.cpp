08d0c38925f27188fa4b5fc5f17f51660da8a715
Rushieksh patil <rushikeshpatil222005@gmail.com>
2026-07-19 02:48:44 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -15,10 +15,12 @@ public:
             if(nums[mid]!= nums[mid-1] && nums[mid]!= nums[mid+1]){
                 return nums[mid];
             }
-
+            // we are on left
             if((mid%2 ==1 && nums[mid]==nums[mid-1]) 
             || (mid%2 ==0 && nums[mid] == nums[mid+1])){
                 low = mid+1;
+
+                // we are on right
             }else{
                 high = mid-1;
             }
